0d3d66560bc9b4ea4ebc78e702fb94f397383a68
magnusa2007 <49444876+magnusa2007@users.noreply.github.com>
2023-03-17 22:31:03 +0100

    Add files via upload

--- Cloud Music/player.lua
+++ Cloud Music/player.lua
@@ -2,23 +2,20 @@ input = {...}
 rs.setOutput("top",true)
 x = 1
 url = input[1]
-file = http.get(url).readAll()
+file = textutils.unserialise(http.get(url).readAll())
 byte = 16*1024
-line = string.sub(file,(x-1)*byte+1,byte+(byte)*(x-1))
 
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
 local decoder = dfpwm.make_decoder()
 function play()
-	while not (line=="") do
-		buffer = decoder(line)
+	for line = 1,#file do
+		buffer = decoder(file[line])
 
 		while not speaker.playAudio(buffer) do
 			os.pullEvent("speaker_audio_empty")
 		end
-		x=x+1
-		line = string.sub(file,(x-1)*byte+1,byte+(byte)*(x-1))
 		if not rs.getOutput("top") then
 			return
 		end
