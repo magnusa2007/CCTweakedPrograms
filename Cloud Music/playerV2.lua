71867f4e0e07b7a55811c16e3be2defacf342351
magnusa2007 <49444876+magnusa2007@users.noreply.github.com>
2023-03-18 19:26:58 +0100

    Add files via upload

--- Cloud Music/playerV2.lua
+++ Cloud Music/playerV2.lua
@@ -2,6 +2,7 @@
 mon = peripheral.find("monitor")
 speaker = peripheral.find("speaker")
 if fs.exists("playlist.table") then
+	print("Found custom playlist")
 	songlist = textutils.unserialize(fs.open("playlist.table","rb").readAll())
 else
 	songlist = textutils.unserialize(http.get("https://raw.githubusercontent.com/magnusa2007/CCTweakedPrograms/main/playlist.table").readAll())
@@ -12,24 +13,29 @@ n=1
 os.startTimer(0.25)
 p="-"
 rs.setOutput("top",false)
-
+pl = 1
 while true do
 	
     mon.clear()
     mon.setCursorPos(1,1)
-	if #songlist[song].name>width then
-		mon.write(songlist[song].name:sub(n,n+width).." | "..songlist[song].name)
+	if #songlist[pl][song].name>width then
+		mon.write(songlist[pl][song].name:sub(n,n+width).." | "..songlist[song].name)
+	else
+		mon.write(songlist[pl][song].name)
 	end
-	mon.write(songlist[song].name)
-    mon.setCursorPos(7, 2)
+    mon.setCursorPos(10, 2)
     mon.write("<   >")
-	mon.setCursorPos(9, 2)
+	mon.setCursorPos(12, 2)
 	if rs.getOutput("top") then
 		p  =  "+"
 	else
 		p=  "-"
 	end
 	mon.write(p)
+	mon.setCursorPos(1, 3)
+	mon.write("Playlist <   >")
+	mon.setCursorPos(1, 4)
+	mon.write(songlist[pl].name)
 	
     event = {os.pullEvent()}
 	--print(textutils.serialize(event))
@@ -37,25 +43,31 @@ while true do
 		x=event[3]
 		y=event[4]
 		if y == 2 then
-			if x == 7 and song > 1 then
+			if x == 10 and song > 1 then
 				song = song-1
 				n=1
-			elseif x == 9 then
+			elseif x == 12 then
 				if rs.getOutput("top") then
 					rs.setOutput("top",false)
 
 				else
 					rs.setOutput("top",true)         
-					shell.run("bg","player.lua", songlist[song].song)
+					shell.run("bg","player.lua", songlist[pl][song].song)
 				end
-			elseif x == 11 and song < #songlist then
+			elseif x == 14 and song < #songlist[pl] then
 				song = song+1
 				n=1
 			end   
+		elseif y==3 then
+			if x == 10 and pl > 1 then
+				pl = pl-1
+			elseif x == 14 and pl < #songlist then
+				pl = pl+1
+			end
 		end
 	end
 	
-	if n>#songlist[song].name+3 then n=1 end
+	if n>#songlist[pl][song].name+3 then n=1 end
 	if event[1] == "timer" then
 		n=n+1
 		os.startTimer(0.5)
