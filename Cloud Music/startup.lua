146cbfbc66f3ec0cc822e60ee933172be833f2b0
magnusa2007 <49444876+magnusa2007@users.noreply.github.com>
2023-03-19 12:57:08 +0100

    Update startup.lua

--- Cloud Music/startup.lua
+++ Cloud Music/startup.lua
@@ -1,3 +1,3 @@
 --Made by MagBot
 --https://github.com/magnusa2007/CCTweakedPrograms
-shell.run("wget","run",https://raw.githubusercontent.com/magnusa2007/CCTweakedPrograms/main/Cloud%20Music/start.lua)
+shell.run("wget","run","https://raw.githubusercontent.com/magnusa2007/CCTweakedPrograms/main/Cloud%20Music/start.lua")
