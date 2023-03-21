e16bd55f19629b8593b8e714bc8e9e62a6a9d2ca
magnusa2007 <49444876+magnusa2007@users.noreply.github.com>
2023-03-21 14:06:59 +0100

    Add files via upload

--- Cloud Music/playerV2.lua
+++ Cloud Music/playerV2.lua
@@ -1,15 +1,15 @@
---Version 2.7
+--Version 2.78
 --Made by MagBot
 --https://github.com/magnusa2007/CCTweakedPrograms
 per = peripheral.getNames()
-mon = term
+mon = table.pack(term)
 speaker = true
 TouchEvent = "mouse_click"
 tabs = "bg"
 for i=1,#per do
 	perT =  peripheral.getType(per[i])
 	if perT == "monitor" then
-		mon = peripheral.find("monitor")
+		monitor = table.pack(peripheral.find("monitor"))
 		TouchEvent = "monitor_touch"
 		tabs = "fg"
 	elseif perT == "speaker" then
@@ -27,13 +27,30 @@ else
 end
 
 song = 1
-width, height = mon.getSize()
+width, height = monitor[1].getSize()
 n=1
 timer = os.startTimer(0.25)
 p="-"
 rs.setOutput("top",false)
 pl = 1
 loop = true
+
+function mon.clear()
+	for i=1,monitor.n do
+		monitor[i].clear()
+	end
+end
+function mon.write(text)
+	for i=1,monitor.n do
+		monitor[i].write(text)
+	end
+end
+function mon.setCursorPos(x,y)
+	for i=1,monitor.n do
+		monitor[i].setCursorPos(x,y)
+	end
+end
+
 while true do
 	
     mon.clear()
@@ -70,7 +87,7 @@ while true do
 		x=event[3]
 		y=event[4]
 		if y == 2 then
-			if x == 10 and song > 1 then
+			if x == 10 then
 				song = song-1
 				n=1
 			elseif x == 12 then
@@ -82,33 +99,40 @@ while true do
 					shell.run(tabs,"player.lua", songlist[pl][song].song)
 					--print(songlist[pl][song].song)
 				end
-			elseif x == 14 and song < #songlist[pl] then
+			elseif x == 14 then
 				song = song+1
 				n=1
 			end   
 		elseif y==4 then
-			if x == 10 and pl > 1 then
+			if x == 10 then
 				pl = pl-1
 			elseif x == 12 then
 				loop = not loop
-			elseif x == 14 and pl < #songlist then
+			elseif x == 14 then
 				pl = pl+1
 			end
 		end
 		os.cancelTimer(timer)
 		timer = os.startTimer(0.25)
+	elseif event[1] == "timer" then
+		n=n+1
+		timer = os.startTimer(0.5)
 	end
+	
+
+	if multishell.getCount() == 1 and rs.getOutput("top") and loop == true then
+		song = song + 1
+	elseif multishell.getCount() == 1 and rs.getOutput("top") and loop == false then
+		shell.run(tabs,"player.lua", songlist[pl][song].song)
+	end
+	if song > #songlist[pl]+0.1  then song = 1 
+	elseif song < 0.9 then song = songlist[pl] end
+	
+	if pl > #songlist+0.1  then pl = 1 
+	elseif pl < 0.9 then pl = songlist end
 	
 	if n>#songlist[pl][song].name+3 then --bug location
 		n=1 
 	end
-	if event[1] == "timer" then
-		n=n+1
-		timer = os.startTimer(0.5)
-	end
-	if multishell.getCount() == 1 and rs.getOutput("top") and loop == true then
-		shell.run(tabs,"player.lua", songlist[pl][song].song)
-	elseif multishell.getCount() == 1 and rs.getOutput("top") and loop == false then
-		rs.setOutput("top",false)
-	end
+
 end
